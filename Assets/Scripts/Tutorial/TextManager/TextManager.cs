89bf54ff8b9cc319b1dfea8ba248385e5461329c
Sacrilege11 <codigoea@gmail.com>
2021-12-11 06:56:55 -0300

    ss
    s

--- Assets/Scripts/Tutorial/TextManager/TextManager.cs
+++ Assets/Scripts/Tutorial/TextManager/TextManager.cs
@@ -8,14 +8,17 @@ public class TextManager : MonoBehaviour
     public Image ColliderMoveTutorial_img;
     public Image ColliderJumpTutorial_img;
     public Image ColliderPuzzleCamSolvetutorial_img;
-   
+    public Image ColliderDestroyCannon_img;
+    public Image ColliderTryMushroom_img;
+
 
     void Start()
     {
         ColliderMoveTutorial_img.enabled = false;
         ColliderJumpTutorial_img.enabled = false;
         ColliderPuzzleCamSolvetutorial_img.enabled = false;
-       
+        ColliderDestroyCannon_img.enabled = false;
+        ColliderTryMushroom_img.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,7 +35,15 @@ public class TextManager : MonoBehaviour
         {
             ColliderPuzzleCamSolvetutorial_img.enabled = true;
         }
-       
+        if (other.gameObject.name == "ColliderDestroyCannon")
+        {
+            ColliderDestroyCannon_img.enabled = true;
+        }
+        if (other.gameObject.name == "ColliderTryMushroom")
+        {
+            ColliderTryMushroom_img.enabled = true;
+        }
+        
     }
 
     private void OnTriggerExit(Collider other)
@@ -48,6 +59,14 @@ public class TextManager : MonoBehaviour
         if (other.gameObject.name == "ColliderPuzzleCamSolvetutorial")
         {
             ColliderPuzzleCamSolvetutorial_img.enabled = false;
+        }
+        if (other.gameObject.name == "ColliderDestroyCannon")
+        {
+            ColliderDestroyCannon_img.enabled = false;
+        }
+        if (other.gameObject.name == "ColliderTryMushroom")
+        {
+            ColliderTryMushroom_img.enabled = false;
         }
     }
 }
